911fbc96e610030a70a2011571e2cc2f3bb63e50
yehonatan2020 <65138695+yehonatan2020@users.noreply.github.com>
2021-12-19 17:31:28 +0000

    fix 1

--- archtitus.sh
+++ archtitus.sh
@@ -2,7 +2,11 @@
     setfont ter-v22b
     bash startup.sh
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/ArchTitus/ArchTitus/1-setup.sh
-    source /mnt/root/ArchTitus/ArchTitus/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/ArchTitus/2-user.sh
-    arch-chroot /mnt /root/ArchTitus/ArchTitus/3-post-setup.sh
+   # arch-chroot /mnt /root/ArchTitus/ArchTitus/1-setup.sh
+   # source /mnt/root/ArchTitus/ArchTitus/install.conf
+   # arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/ArchTitus/2-user.sh
+   # arch-chroot /mnt /root/ArchTitus/ArchTitus/3-post-setup.sh
+   arch-chroot /mnt /root/$SCRIPTHOME/1-setup.sh
+    source /mnt/root/$SCRIPTHOME/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/$SCRIPTHOME/2-user.sh
+    arch-chroot /mnt /root/$SCRIPTHOME/3-post-setup.sh
