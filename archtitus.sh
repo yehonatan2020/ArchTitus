cdfb980e9d22762b7c9c270031d9f4b1b214b796
yehonatan2020 <65138695+yehonatan2020@users.noreply.github.com>
2021-12-29 09:10:45 +0000

    fix script

--- archtitus.sh
+++ archtitus.sh
@@ -4,7 +4,7 @@ export SCRIPTHOME="$(basename -- $PWD)"
     setfont ter-v22b
     bash startup.sh
     bash 0-preinstall.sh
-   arch-chroot /mnt /root/$SCRIPTHOME/$SCRIPTHOME/1-setup.sh
     source /mnt/root/$SCRIPTHOME/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/$SCRIPTHOME/$SCRIPTHOME/2-user.sh
-    arch-chroot /mnt /root/$SCRIPTHOME/$SCRIPTHOME/3-post-setup.sh
+    SCRIPTHOME=$SCRIPTHOME arch-chroot /mnt /root/$SCRIPTHOME/1-setup.sh
+    SCRIPTHOME=$SCRIPTHOME arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/$SCRIPTHOME/2-user.sh
+    SCRIPTHOME=$SCRIPTHOME arch-chroot /mnt /root/$SCRIPTHOME/3-post-setup.sh
