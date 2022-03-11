412f91f1fc969b999a15194f36d7195c12e0c0d1
yehonatan2020 <65138695+yehonatan2020@users.noreply.github.com>
2022-03-11 02:59:56 +0000

    removing swapfile

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -172,21 +172,22 @@ if [[ ! -d "/sys/firmware/efi" ]]; then
 else
     pacstrap /mnt efibootmgr --noconfirm --needed --color=always
 fi
-echo -ne "
--------------------------------------------------------------------------
-                    Creating Swap File
--------------------------------------------------------------------------
-"
-    # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
-    mkdir -p /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
-    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
-    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=8192 status=progress
-    chmod 600 /mnt/opt/swap/swapfile # set permissions.
-    chown root /mnt/opt/swap/swapfile
-    mkswap /mnt/opt/swap/swapfile
-    swapon /mnt/opt/swap/swapfile
-    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
-    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
+#echo -ne "
+
+#-------------------------------------------------------------------------
+  #                  Creating Swap File
+#-------------------------------------------------------------------------
+#"
+ #   # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
+#    mkdir -p /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
+#    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
+ #   dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=8192 status=progress
+#    chmod 600 /mnt/opt/swap/swapfile # set permissions.
+ #   chown root /mnt/opt/swap/swapfile
+#    mkswap /mnt/opt/swap/swapfile
+#    swapon /mnt/opt/swap/swapfile
+ #   # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
+#    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab # Add swap to fstab, so it KEEPS working after installation.
 echo -ne "
 -------------------------------------------------------------------------
                     SYSTEM READY FOR 1-setup.sh
